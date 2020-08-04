26f4b564d165ee2c131850ce8f94cd86c7039138
Do Chanh Tin <@gmail>
2020-08-04 11:10:00 +0700

    Update

---
+++ LoopInJava/BT-InCacSoNguyenToNhoHon100/src/PrintPrimesSmaller100.java
@@ -0,0 +1,15 @@
+public class PrintPrimesSmaller100 {
+    public static void main(String[] args) {
+        boolean check=true;
+
+        for(int i=2;i<100;i++){
+            for (int j=2;j<i;j++) {
+                if (i % j == 0) {
+                    check=false;
+                    break;
+                } else check = true;
+            }
+            if(check) System.out.printf("%d ",i);
+        }
+    }
+}
