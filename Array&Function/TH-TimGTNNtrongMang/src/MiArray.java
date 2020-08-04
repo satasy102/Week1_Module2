c3b9f1c7c4b5894eb4d080a537d344568d1c8ee2
Do Chanh Tin <@gmail>
2020-08-04 16:46:36 +0700

    Update

---
+++ Array&Function/TH-TimGTNNtrongMang/src/MiArray.java
@@ -0,0 +1,19 @@
+public class MiArray {
+    public static void main(String[] args) {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array){
+        int min=array[0];
+        byte index=0;
+        for(byte i=1;i<array.length;i++){
+            if(min>array[i]) {
+                min=array[i];
+                index= i;
+            }
+        }
+        return index;
+    }
+}
